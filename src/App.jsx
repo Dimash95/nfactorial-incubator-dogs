9dc2a97ad8d4f5d69506a2b415a5672b9e0fbd0c
Dimash <dinmukhamed.amirov@gmail.com>
2024-04-15 16:44:12 +0500

    feat: add category

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const fetchData = async () => {
+  const response = await axios.get("https://dog.ceo/api/breeds/list/all");
+  return response.data.message;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [category, setCategory] = useState({});
+
+  const displayData = async () => {
+    const fetchedData = await fetchData();
+    setCategory(fetchedData);
+    console.log(fetchedData);
+  };
+
+  useEffect(() => {
+    displayData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl">Dogs</h1>
+      <div className="flex justify-around mt-10">
+        <div className=" w-1/2">
+          <h2 className="text-red-500">Catalog</h2>
+          <div>
+            {Object.keys(category).map((key) => (
+              <p key={key} className="flex gap-2">
+                <p>{key}:</p>
+                <p className="flex flex-wrap gap-2">
+                  {category[key].map((sub) => (
+                    <p key={sub}>{sub}, </p>
+                  ))}
+                </p>
+              </p>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Image</h2>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
